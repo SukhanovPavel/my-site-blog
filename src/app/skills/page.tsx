ef80d398986350debee37bdfa5de09ad906f7f27
Pavel Sukhanov <psuhanov77@gmail.com>
2023-06-13 12:43:37 +0300

    add pages

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,114 @@
+import Image from 'next/image';
+import Link from "next/link";
+
+import styles from '../page.module.css';
+
+export default function Skills() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <p>
+                    Hi! I'm Junior Frontend developer.
+                    {/*<code className={styles.code}>src/app/page.tsx</code>*/}
+                </p>
+                <div>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        By{' '}
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            className={styles.vercelLogo}
+                            width={100}
+                            height={24}
+                            priority
+                        />
+                    </a>
+                </div>
+            </div>
+
+            <Link href="/">
+                <div className={styles.center}>
+                    <Image
+                        className={styles.logo}
+                        src="/next.svg"
+                        alt="Next.js Logo"
+                        width={180}
+                        height={37}
+                        priority
+                    />
+                    My skills
+                    <div className={styles.centerDesc}>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                        <p>React</p>
+                    </div>
+                </div>
+            </Link>
+
+            <div className={styles.grid}>
+                <Link
+                    href="/skills"
+                    className={styles.card}
+                    // target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Skills <span>-&gt;</span>
+                    </h2>
+                    <p>My skills</p>
+                </Link>
+
+                <Link
+                    href="portfolio"
+                    className={styles.card}
+                    // target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Portfolio <span>-&gt;</span>
+                    </h2>
+                    <p>My works</p>
+                </Link>
+
+                <Link
+                    href="web3"
+                    className={styles.card}
+                    // target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Web3 <span>-&gt;</span>
+                    </h2>
+                    <p>My hobby</p>
+                </Link>
+
+                <Link
+                    href="blog"
+                    className={styles.card}
+                    // target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Blog <span>-&gt;</span>
+                    </h2>
+                    <p>
+                        Coming soon...
+                    </p>
+                </Link>
+            </div>
+        </main>
+    )
+}
