90c11fc0c94c446b23f7a11da7995de73c119232
Pavel Sukhanov <psuhanov77@gmail.com>
2024-01-11 23:19:06 +0300

    error.error

--- src/components/create-new-user.tsx
+++ src/components/create-new-user.tsx
@@ -49,7 +49,7 @@ export const CreateNewUser = ({handleClose}: Props) => {
             }
         } catch (error) {
             // Обработка сетевой ошибки
-            if(error) {
+            if(error.error) {
                 alert(error?.error)
             }
         }
