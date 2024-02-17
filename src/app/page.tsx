2ed687d810823fade7c7bde223f2b2bcd693a638
Pavel Sukhanov <psuhanov77@gmail.com>
2024-02-17 20:35:43 +0300

    delete campervanpage (test)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import {Main} from "@/components/Main";
 import {Metadata} from "next";
-import CamperVanPage from "@/components/CamperVanPage";
 
 export const metadata: Metadata = {
     title: 'Блог фронтенд разработчика о фронтенд разработке',
@@ -12,7 +11,6 @@ export default function Home() {
   return (
     <>
         <Main />
-        <CamperVanPage />
     </>
   )
 }
--- src/components/CamperVanPage.tsx
+++
@@ -1,6 +0,0 @@
-import { auth } from "../../auth"
-
-export default async function Page() {
-    // const { user } = await auth()
-    // return <p>Hello {user?.name}</p>
-}
